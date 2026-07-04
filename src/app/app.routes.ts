56fc3d445ad8ad97b2f37d3658b8789d65f22ef0
Cesar David Rodriguez Mora <cesarpet6@gmail.com>
2026-07-03 22:58:03 -0500

    feat: crear estructura de inicio de sesion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage)
+  },
+    {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage),
   },
 ];
